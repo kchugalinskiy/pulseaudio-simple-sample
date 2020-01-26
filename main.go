a7a4080db450719bb0337e31a915fb75ecdde513
Konstantin Chugalinskiy <kchugalinskiy@yandex.ru>
2020-01-26 23:51:39 +0300

    Add simple API echo sample

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/kchugalinskiy/pulseaudio/simple"
+
+	"github.com/gotk3/gotk3/gtk"
+)
+
+func main() {
+	gtk.Init(nil)
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal("Не удалось создать окно:", err)
+	}
+
+	exitCh := make(chan interface{})
+	defer close(exitCh)
+
+	win.SetTitle("Простой пример")
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+		close(exitCh)
+	})
+
+	f := simple.SampleSpec{
+		Format:   simple.SampleFormatS32LE,
+		Rate:     44100,
+		Channels: 1,
+	}
+	play, err := simple.New("", "echo example play", simple.StreamDirectionPlayback, "", "Music", &f, nil, nil)
+	if err != nil {
+		fmt.Printf("creating sample play: %v\n", err)
+		return
+	}
+	defer play.Close()
+
+	rec, err := simple.New("", "echo example rec", simple.StreamDirectionRecord, "", "Music", &f, nil, nil)
+	if err != nil {
+		fmt.Printf("creating sample rec: %v\n", err)
+		return
+	}
+	defer rec.Close()
+
+	go func() {
+		for {
+			select {
+			case <-exitCh:
+				return
+			default:
+				b, err := rec.Read(1024)
+				if err != nil {
+					fmt.Printf("reading audio: %v\n", err)
+					continue
+				}
+
+				if err = play.Write(b); err != nil {
+					fmt.Printf("writing audio: %v\n", err)
+					continue
+				}
+			}
+		}
+	}()
+
+	gtk.Main()
+}
